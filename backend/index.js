a7fcea2306fc7a05f882e9613f0f9c0a636e5d93
mahi25soni <msdraj02@protonmail.com>
2024-04-24 19:51:51 +0530

    lsiting task done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("dotenv").config() 
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 
 const dbConnect = require("./config/mongoDb")
@@ -10,6 +11,7 @@ const taskRoute = require("./route/taskRoute")
 const port = process.env.PORT || 3000
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(cookieParser())
--- frontend/src/components/GetToDo.jsx
+++ frontend/src/components/GetToDo.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function GetToDo() {
-    console.log("get to do")
+    
+    const [todo , setTodo] = useState([])
 
-  return (
-    <> 
-        <p>task details</p>
-        <p>description</p>
+    useEffect(() => {
+        fetch("http://localhost:3000/api/task/")
+        .then((response) => {
+            response.json()
+            .then((final) => {
+                setTodo(final.data)
+            })
+        })
+        .catch(err => {
+            console.log("The error is " , err)
+        })
+    })
+
+    return (
+        <> 
+        {todo.map(value => {
+
+        })}
+
+        </>
+    )
+}
+
+function ListingTask(props) {
+    
+    return (
+    <div>
+        <p>{value.title}</p>
+        <p>{value.description}</p>
         <label htmlFor="markdone">Mark</label>
-        <input type="radio" name="markdone" id="" />
-    </>
-  )
+        <input type="radio" name="markdone" id="" onChange={} />    
+    </div>
+    )
+
 }
